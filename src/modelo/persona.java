9ac139295602abf15a3ff1a28dd45515d24f6594
Julian Benalcazar <jbenalcazar@MacBook-Air-de-Amapola.local>
2025-04-27 20:17:31 -0500

    update model persona

--- src/modelo/persona.java
+++ src/modelo/persona.java
@@ -1,42 +1,94 @@
 package modelo;
 
-public class persona {
-    private String nombre, telefono, email, categoria;
+public class Persona {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String categoria;
     private boolean favorito;
-    
-    public persona() {
-        this.nombre = "";
-        this.telefono = "";
-        this.email = "";
-        this.categoria = "";
-        this.favorito = false;
+
+    // Constructor vacío
+    public Persona() {
     }
-    
-    public persona(String nombre, String telefono, String email, String categoria, boolean favorito) {
+
+    // Constructor con todos los campos
+    public Persona(int id, String nombre, String telefono, String email, String categoria, boolean favorito) {
+        this.id = id;
         this.nombre = nombre;
         this.telefono = telefono;
         this.email = email;
         this.categoria = categoria;
         this.favorito = favorito;
     }
-    
-    // Getters y Setters
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getCategoria() { return categoria; }
-    public void setCategoria(String categoria) { this.categoria = categoria; }
-    public boolean isFavorito() { return favorito; }
-    public void setFavorito(boolean favorito) { this.favorito = favorito; }
-    
-    public String datosContacto() {
-        return String.format("%s;%s;%s;%s;%s", nombre, telefono, email, categoria, favorito);
+
+    // Constructor sin id para nuevos contactos
+    public Persona(String nombre, String telefono, String email, String categoria, boolean favorito) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.categoria = categoria;
+        this.favorito = favorito;
     }
-    
-    public String formatoLista() {
-        return String.format("%-40s%-40s%-40s%-40s", nombre, telefono, email, categoria);
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", favorito=" + favorito +
+                '}';
     }
 }
